fba7bd974b8a6952b3472925e4f761c4dd624f23
Bagasprawira Herlambang <bagasprawirarh@gmail.com>
2022-06-17 18:55:31 +0200

    edited

--- Assesment/get_data_image.py
+++ Assesment/get_data_image.py
@@ -28,32 +28,36 @@ Dist_rout = []
 i = 0
 Lat_prev = 0
 Lon_prev = 0
+DR_tot = 0
 for img in images_list:
     Lat_list.append(img.lat)
     Lon_list.append(img.lon)
-    Hdg_list.append(img.yaw)
+    Hdg_list.append(img.yaw+180)
     Alt_list.append(img.alt)
     p1 = (Lat_prev,Lon_prev)
     p2 = (img.lat,img.lon)
     pr = (52.9438215,12.7850094)
     OV,Dist = get_overlap(p1,p2,img.alt)
     _, DR = get_overlap(pr,p2,img.alt)
+    DR_tot += DR
     Dist_rout.append(DR)
     OV_list.append(OV)
     Lat_prev = img.lat
     Lon_prev = img.lon
     data2txt(PATH_TXT, filename, round(img.lat, 7), round(img.lon, 7), round(img.alt, 3) ,round(img.yaw, 3),OV,Dist)
 
-print(np.mean(OV_list[1:-1]),np.mean(Alt_list),np.mean(Dist_rout))
+Time_s = DR_tot/20
+print(Time_s)
+#print(np.mean(OV_list[1:-1]),np.mean(Alt_list),np.mean(Dist_rout), DR_tot)
 #creating plot Lat Lon
-fig1 = plt.figure(figsize=(12,12))
+fig1 = plt.figure(figsize=(14,12))
 plt.rcParams.update({'font.size': 20})
-plt.quiver(Lon_list, Lat_list, np.cos(np.radians(Hdg_list)), np.sin(np.radians(Hdg_list)), label = 'geografische Position der Bildaufnahme', width = 0.005, color='r')
-plt.xlabel('Längengrad')
-plt.ylabel('Breitengrad')
+plt.quiver(Lat_list, Lon_list, np.cos(np.radians(Hdg_list)), np.sin(np.radians(Hdg_list)), label = 'geografische Position der Bildaufnahme', width = 0.005, color='r')
+plt.xlabel('Breitengrad')
+plt.ylabel('Längengrad')
 plt.legend(loc = 'lower left')
-fig1.savefig(PATH_TXT + '\\' + filename + '_LatLon.jpg')
-fig1.savefig(PATH_TXT + '\\' + filename + '_LatLon.pgf')
+fig1.savefig(PATH_TXT + '\\' + filename + '_LonLat.jpg')
+fig1.savefig(PATH_TXT + '\\' + filename + '_LonLat.pgf')
 plt.close(fig1)
 
 # #creating plot Alt
